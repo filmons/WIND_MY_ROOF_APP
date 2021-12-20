97bd98ca88cd8c02a13175cc4ce401f750569ee9
filmons <filmonseare@gmail.com>
2021-12-20 07:29:39 +0100

    Correction 🚀🚀🚀🚀

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -1,17 +1,12 @@
-const express = require("express");
+const express    = require("express");
 const userRouter = express.Router();
-
-
 
 
 const UserController = require("../controllers/user_controller");
 
 userRouter.get("/users", UserController.findAllUsers);
 userRouter.get("/user/:id",UserController.OneUsers);
-// userRouter.get("/user/:role",UserController.findByRole);
-
 userRouter.post("/signup",UserController.newUser);
-
 userRouter.post("/login",UserController.findUser);
 
 module.exports = userRouter;
